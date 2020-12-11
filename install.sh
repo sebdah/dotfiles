3c80144825b95391596e6e213866bce8b7762394
Sebastian Dahlgren <sebastian.dahlgren@gmail.com>
2020-12-11 20:34:52 +0100

    Move files to their dot representation

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+ln -s $(pwd)/.agignore ${HOME}/.agignore
+ln -s $(pwd)/.config/configstore ${HOME}/.config/configstore
+ln -s $(pwd)/.config/direnv ${HOME}/.config/direnv
+ln -s $(pwd)/.config/fish ${HOME}/.config/fish
+ln -s $(pwd)/.config/git ${HOME}/.config/git
+ln -s $(pwd)/.config/gocode ${HOME}/.config/gocode
+ln -s $(pwd)/.config/htop ${HOME}/.config/htop
+ln -s $(pwd)/.config/hub ${HOME}/.config/hub
+ln -s $(pwd)/.config/kitty ${HOME}/.config/kitty
+ln -s $(pwd)/.config/nvim ${HOME}/.config/nvim
+ln -s $(pwd)/.gitconfig ${HOME}/.gitconfig
+ln -s $(pwd)/.gitignore.global ${HOME}/.gitignore
+ln -s $(pwd)/.tmux ${HOME}/.tmux
+ln -s $(pwd)/.tmux.conf ${HOME}/.tmux.conf
+ln -s $(pwd)/.urlview ${HOME}/.urlview
