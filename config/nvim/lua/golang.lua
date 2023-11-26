99e16fd2b034d55543f9a4317e98acacbdfbba17
Sebastian Dahlgren <sebastian.dahlgren@gmail.com>
2023-11-26 21:05:06 +0100

    [nvim] Add Golang LUA config

--- config/nvim/lua/golang.lua
+++ config/nvim/lua/golang.lua
@@ -1,1 +1,13 @@
 require('go').setup()
+
+-- Run gofmt + goimport on save
+
+local format_sync_grp = vim.api.nvim_create_augroup("GoImport", {})
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*.go",
+  callback = function()
+   require('go.format').goimport()
+  end,
+  group = format_sync_grp,
+})
+
